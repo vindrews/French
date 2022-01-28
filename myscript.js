c7ca4edfcfd2a44174c0c1281629811791b77153
P1 <you@example.com>
2022-01-28 09:55:37 -0800

    did so much stuff

--- myscript.js
+++ myscript.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function(){
+  $(".wrong").click(function(){
+    $(".wrong").css({"background-color": "red"});
+      alert("Non!");
+    });
+		$(".right").click(function(){
+		$(".right").css({"background-color": "lime"});
+		$("#next").css({"display": "block"});
+		  alert("Oui!");
+		});
+			$(".right").click(function(){
+});
