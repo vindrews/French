24fef23a834fdec405e986d3c56e217b544c89b0
P1 <you@example.com>
2022-01-31 09:52:16 -0800

    did so much lol

--- myscript.js
+++ myscript.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
-  $(".wrong").click(function(){
-    $(".wrong").css({"background-color": "red"});
-      alert("Non!");
-    });
-		$(".right").click(function(){
-		$(".right").css({"background-color": "lime"});
-		$("#next").css({"display": "block"});
-		  alert("Oui!");
-		});
-			$(".right").click(function(){
+	for(let n=1; n <=10; n++)
+	{
+    $(".wrong"+n).click(function(){
+      $(".wrong"+n).css({"background-color": "red"});
+        alert("Non!");
 });
+
+	$(".right"+n).click(function(){
+	  $(".right"+n).css({"background-color": "lime"});
+	    $(".go"+n).css({"display":"block"});
+	      alert("Oui!");
+});
+
+	$(".go1").click(function(){
+	  $(".q1").css({"display": "block"});
+});
+
+	$(".go2").click(function(){
+	  $(".q2").css({"display": "block"});
+});
+
+	$(".go3").click(function(){
+	  $(".q3").css({"display": "block"});
+});
+
+	$(".go4").click(function(){
+	  $(".q4").css({"display": "block"});
+});
+
+	$(".go5").click(function(){
+	  $(".q5").css({"display": "block"});
+});
+
+	}
 });
